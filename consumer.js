7a3e4e0b8c90cc866391bcd0d249339ffd1d1f79
jaiswalsourav <jaiswalsourav060@gmail.com>
2026-07-03 18:41:01 +0530

    your message here

--- consumer.js
+++ consumer.js
@@ -3,7 +3,7 @@ const group = process.argv[2];
 
 async function init() {
   const consumer = kafka.consumer({ groupId: "user-1" });
-  await consumer.connect();
+  //await consumer.connect();
 
   await consumer.subscribe({ topics: ["rider-updates"], fromBeginning: true });
 
